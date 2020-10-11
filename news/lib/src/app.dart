add8f4f6645fd9d3eca91d9b29759a22bbbd990a
ARTQ\GorodeckayaM <GorodeckayaM@quantumart.ru>
2020-10-11 23:56:04 +0300

    navigation and news detail screen have been added

--- news/lib/src/app.dart
+++ news/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
+import 'screens/news_detail.dart';
 import 'blocs/stories_provider.dart';
 
 class App extends StatelessWidget {
@@ -8,7 +9,25 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'News!',
         home: NewsList(),
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) {
+          final itemId = int.parse(settings.name.replaceFirst('/', ''));
+          return NewsDetail(itemId: itemId);
+        },
+      );
+    }
   }
 }
